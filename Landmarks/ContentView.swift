26af6445b5aa82a39466ef53c18b1c1f4970f4f5
Brian Moscosa <bmoscosa@cps.edu>
2024-04-25 08:35:38 -0500

    4/25/24

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,15 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, me!")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
         .padding()
     }
 }
+
+//Section 1
+/*
+ var body: some view {
+ Text("Hello SwiftUI!")
+ }
+ }
+*/
+//Section 2
+/*
+var body: some view {
+ Text("Turtle Rock")
+ .font(.title)
+ .foregroundColor(.green)
+ }
+ }
+*/
+
+//Section 3
+/*
+var body: some View {
+VStack(alignment: .leading) {
+    Text("Turtle Rock")
+        .font(.title)
+    HStack {
+        Text("Joshua Tree National Park")
+            .font(.subheadline)
+        Spacer()
+        Text("California")
+            .font(.subheadline)
+    }
+}
+.padding()
+}
+}
+*/
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
